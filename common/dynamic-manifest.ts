fb2ee2df847ea78b98b8c4086dff0b9ad6824c9d
NaitLee <naitli@foxmail.com>
2024-01-04 14:31:33 +0800

    better pwa: re-add screenshots, add maskable icon

--- common/dynamic-manifest.ts
+++ common/dynamic-manifest.ts
@@ -1,7 +1,8 @@
 
 import { _ } from "./i18n.tsx";
 
-export function manifest() {
+export function dynamicManifest() {
+    // origin for url is needed for such generated inline manifest
     const origin = location ? location.origin : '';
     const is_dark_theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const theme_color = is_dark_theme ? '#202020' : '#fdfdfd';
@@ -16,8 +17,24 @@ export function manifest() {
         "orientation": "any",
         "icons": [{
             "src": origin + "/kitty.png",
-            "sizes": "1024x1024",
+            "sizes": "32x32 48x48 72x72 96x96 128x128 256x256 512x512 1024x1024",
             "type": "image/png"
+        }, {
+            "src": origin + "/kitty-maskable.png",
+            "sizes": "32x32 48x48 72x72 96x96 128x128 256x256 512x512 1024x1024",
+            "type": "image/png",
+            "purpose": "maskable"
+        }],
+        "screenshots": [{
+            "src": origin + (is_dark_theme ? "/screenshots/narrow-dark.png" : "/screenshots/narrow.png"),
+            "type": "image/png",
+            "sizes": "720x1280",
+            "form_factor": "narrow"
+        }, {
+            "src": origin + (is_dark_theme ? "/screenshots/wide-dark.png" : "/screenshots/wide.png"),
+            "type": "image/jpg",
+            "sizes": "1280x720",
+            "form_factor": "wide"
         }]
     };
 }
--- islands/DynamicManifest.tsx
+++ islands/DynamicManifest.tsx
@@ -1,14 +1,19 @@
 
 import { useEffect } from "preact/hooks";
-import { manifest } from "../common/dynamic-manifest.ts";
+import { dynamicManifest } from "../common/dynamic-manifest.ts";
 import { i18nReady } from "../common/i18n.tsx";
 
 export default function DynamicManifest() {
     useEffect(() => {
         i18nReady(function () {
+            const manifest = dynamicManifest();
+            const meta = document.createElement('meta');
+            meta.name = 'theme-color';
+            meta.content = manifest.theme_color;
+            document.head.appendChild(meta);
             const link = document.createElement('link');
             link.rel = 'manifest';
-            link.href = 'data:application/manifest+json,' + encodeURIComponent(JSON.stringify(manifest()));
+            link.href = 'data:application/manifest+json,' + encodeURIComponent(JSON.stringify(manifest));
             document.head.appendChild(link);
         })
     });
