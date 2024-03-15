711bdb3d1a722ded5b8e650cc885bdd4a607a64e
NaitLee <naitli@foxmail.com>
2024-03-15 21:40:39 +0800

    serve https if there is `ca.crt` and `ca.key`

--- main.ts
+++ main.ts
@@ -9,4 +9,18 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-await start(manifest);
+const cert_file = 'ca.crt';
+const key_file = 'ca.key';
+
+const cert = await Deno.readTextFile(cert_file).catch(() => '');
+const key = await Deno.readTextFile(key_file).catch(() => '');
+
+if (cert !== '' && key !== '')
+    await start(manifest, {
+        server: {
+            cert: cert,
+            key: key
+        }
+    });
+else
+    await start(manifest);
